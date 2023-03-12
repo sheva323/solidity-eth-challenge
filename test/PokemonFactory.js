11bf4a198c1ae0c73c4c5a71623e034865c45a39
sheva323 <sheva3232@gmail.com>
2023-03-12 09:56:26 -0500

    unit test creating 2 pokemons working good

--- test/PokemonFactory.js
+++ test/PokemonFactory.js
@@ -11,9 +11,11 @@ describe("Pokemon Factory Contract", function (){
   it("Pokemon Factory Should create pokemons ", async function() {
     const PokemonFactory = await ethers.getContractFactory("PokemonFactory");
     const hardhatPokemon = await PokemonFactory.deploy();
-    const pokemons = await hardhatPokemon.createPokemon("test1", 1, skills);
+    const pokemons = await hardhatPokemon.createPokemon("test1", 1);
+    const pokemons2 = await hardhatPokemon.createPokemon("test2", 2);
     console.log(pokemons);
+    console.log(pokemons2);
     const getPokemonst = await hardhatPokemon.getAllPokemons();
-    expect(getPokemonst.length).to.equal(1);
+    expect(getPokemonst.length).to.equal(2);
   });
 })
